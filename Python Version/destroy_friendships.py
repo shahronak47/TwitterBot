350f3d7c99354faa366ac133af056d01bed15bcd
Ronak Shah <shahronak47@yahoo.in>
2017-07-22 00:02:52 +0800

    Deleting all files

--- Python Version/destroy_friendships.py
+++
@@ -1,7 +0,0 @@
-from setupTwythonAuthorization import twython_authorization
-
-def destroyFriendships(unfollowIds) :
-    api = twython_authorization()
-    for id in unfollowIds :
-        api.destroy_friendship(user_id = id)
-
--- README.md
+++
@@ -1,3 +0,0 @@
-# TwitterBot
-
-This code randomly finds people on Twitter using #GoodMorning and replies them back wishing them Good Morning. 
