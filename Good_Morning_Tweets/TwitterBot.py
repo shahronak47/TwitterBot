4cfad733bff19f5377debc4f09cf445f922fbcf4
Ronak Shah <shahronak47@yahoo.in>
2018-05-18 22:02:51 +0800

    Follow/unfollow max 10 people at a time

---
+++ Good_Morning_Tweets/TwitterBot.py
@@ -0,0 +1,40 @@
+from setupTwitterAuthorization import setupTwitterAutorization
+from create_friendships import create_frienships
+from destroy_friendships import destroyFriendships
+import pdb
+
+if __name__ == "__main__":
+
+    #Setting up twitter authorization
+    api = setupTwitterAutorization('keys.yml')
+    try: 
+    #Search for #GoodMorning
+        searchResult = api.search('#GoodMorning -filter:retweets', count = 1)
+        #Take only first tweet
+        if not not searchResult:
+            tweet = searchResult[0]
+            if not tweet.retweeted :
+                api.update_status("@" + tweet.user.screen_name + " Good Morning, Have a nice day! :-) ", tweet.id)
+
+    except Exception as error :
+        #Handle duplicate status exception
+            if error[0][0]['code'] == 187 :
+                tweet = searchResult[1]
+                api.update_status("@" + tweet.user.screen_name + " Good Morning, Have a nice day! :-) ", tweet.id)
+        #Get the followers
+        followerIds = api.followers_ids()
+        friendIds = api.friends_ids()
+        #Get people to follow
+        followIds = list(set(followerIds) - set(friendIds))
+        #Follow max of 10 people
+        followIds = followIds[:10]
+        #Get people to unfollow
+        unfollowIds = list(set(friendIds) - set(followerIds))
+        # Unfollow max of 10 people
+        unfollowIds = unfollowIds[:10]
+
+if not not followIds :
+            create_frienships(followIds)
+
+        if not not unfollowIds :
+            destroyFriendships(unfollowIds)
